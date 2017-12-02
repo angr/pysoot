11e7f4c1bf99f0ecf3d0da35c9d5e6043dcbce44
Fish <fish@cs.ucsb.edu>
2017-12-01 23:55:44 -0800

    set version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='pysoot',
-    version='1.3',
+    version='7.7.12.1',
     description='Get Shimple/Jimple IR in Python',
     packages=['pysoot'],
     install_requires=['psutil'],
